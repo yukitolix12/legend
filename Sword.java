cec3654dd5d380c39ac5337275acfc5af0097f27
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-08-11 10:53:40 +0900

    Sword詳細

--- Sword.java
+++ Sword.java
@@ -6,4 +6,17 @@ public  class Sword implements Cloneable {
         result.name = this.name;
         return result;
     }
+    
+    public Sword(){
+        this.name = "";
+    }
+    public Sword(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
 }
